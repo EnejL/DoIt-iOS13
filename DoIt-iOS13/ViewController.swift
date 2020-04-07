41862f5176f5c95334660ac9f0f81d9d1866e40d
Enej <enej@Enejs-MacBook-Pro.local>
2020-04-07 21:31:57 +0200

    first commit

---
+++ DoIt-iOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt-iOS13
+//
+//  Created by Enej on 07/04/2020.
+//  Copyright © 2020 Enej. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
